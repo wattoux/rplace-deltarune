2ca460270bd9cc2189f152e454a8d43a8a786ef0
9bg <99905695+wattoux@users.noreply.github.com>
2023-07-22 03:18:10 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,23 @@
+{
+  "contact": "r/deltarune - https://discord.gg/AuqARRvnC4",
+  "templates": [
+    {
+      "name": "Ralsei USA Flag",
+      "sources": [
+        "https://media.discordapp.net/attachments/944593144798932992/1132110037599399996/RALSEI.png"
+      ],
+      "x": 411,
+      "y": 663
+    },
+    {
+      "name": "Toby Fox",
+      "sources": [
+        "https://media.discordapp.net/attachments/1131353707053846550/1132117289987211264/toby.png"
+      ],
+      "x": 1271,
+      "y": 712
+    }
+  ],
+  "whitelist": [],
+  "blacklist": []
+}
