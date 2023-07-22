c9891f4766548413377e3ad6cda55a2a68ee24d9
9bg <99905695+wattoux@users.noreply.github.com>
2023-07-22 02:45:29 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,15 @@
+{
+  "contact": "r/deltarune - https://discord.gg/AuqARRvnC4",
+  "templates": [
+    {
+      "name": "ralsei",
+      "sources": [
+        "https://media.discordapp.net/attachments/944593144798932992/1132110037599399996/RALSEI.png"
+      ],
+      "x": 411,
+      "y": 663
+    }
+  ],
+  "whitelist": [],
+  "blacklist": []
+}
