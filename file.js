912697c8a15d68bb82657966b0f3ff490e6e83f3
9bg <99905695+wattoux@users.noreply.github.com>
2023-07-24 00:25:17 +0200

    Update file.js

--- file.js
+++ file.js
@@ -1,13 +1,13 @@
 {
-  "contact": "9bG + Thethree13",
+  "contact": "9bG + Thethree13 r/deltarune",
   "templates": [
     {
       "name": "deltarune",
       "sources": [
-        "https://media.discordapp.net/attachments/944593144798932992/1132794377740222474/DOWNSCALING_TEST.png"
+        "https://media.discordapp.net/attachments/944593144798932992/1132800475763331102/DOWNSCALING_TEST.png"
       ],
       "x": 1327,
-      "y": 1924
+      "y": 1920
     }
   ],
   "whitelist": [],
