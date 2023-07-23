8ac1c334e17cdcfd3e7390173d7a253160d87e2b
9bg <99905695+wattoux@users.noreply.github.com>
2023-07-24 00:36:11 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,15 @@
+{
+  "contact": "9bG + Thethree13 r/deltarune",
+  "templates": [
+    {
+      "name": "deltarune",
+      "sources": [
+        "https://media.discordapp.net/attachments/944593144798932992/1132800475763331102/DOWNSCALING_TEST.png"
+      ],
+      "x": 1323,
+      "y": 1924
+    }
+  ],
+  "whitelist": [],
+  "blacklist": []
+}
