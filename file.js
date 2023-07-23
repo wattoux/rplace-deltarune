5dc32d713fab3337dec0a745e54d97780adec71e
9bg <99905695+wattoux@users.noreply.github.com>
2023-07-24 00:00:52 +0200

    Update file.js

--- file.js
+++ file.js
@@ -4,10 +4,10 @@
     {
       "name": "deltarune",
       "sources": [
-        "https://media.discordapp.net/attachments/944593144798932992/1132791252308471848/DOWNSCALING_TEST.png"
+        "https://media.discordapp.net/attachments/944593144798932992/1132794377740222474/DOWNSCALING_TEST.png"
       ],
       "x": 1327,
-      "y": 1925
+      "y": 1924
     }
   ],
   "whitelist": [],
