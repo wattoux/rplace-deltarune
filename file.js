b7d1b8247f8ceeb483cce6d375d605369ebb88e7
9bg <99905695+wattoux@users.noreply.github.com>
2023-07-23 21:54:38 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,15 @@
+{
+  "contact": "9bg",
+  "templates": [
+    {
+      "name": "deltarune",
+      "sources": [
+        "https://cdn.discordapp.com/attachments/1131625446711242873/1132762331760304179/DOWNSCALING_TEST.png"
+      ],
+      "x": 1327,
+      "y": 1924
+    }
+  ],
+  "whitelist": [],
+  "blacklist": []
+}
