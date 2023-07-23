1a02ed59be8d954649f2e19bf0c3a3827fe62d4d
9bg <99905695+wattoux@users.noreply.github.com>
2023-07-23 20:18:08 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,15 @@
+{
+  "contact": "9bg",
+  "templates": [
+    {
+      "name": "deltarune",
+      "sources": [
+        "https://media.discordapp.net/attachments/944593144798932992/1132738252021977168/deltar_jeee.png"
+      ],
+      "x": 827,
+      "y": 1424
+    }
+  ],
+  "whitelist": [],
+  "blacklist": []
+}
