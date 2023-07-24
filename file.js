3758c1cbb844693a734b06bb7f680243b3676a34
9bg <99905695+wattoux@users.noreply.github.com>
2023-07-24 16:21:19 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,23 @@
+{
+  "contact": "9bG + Thethree13 r/deltarune",
+  "templates": [
+    {
+      "name": "deltarune",
+      "sources": [
+        "https://media.discordapp.net/attachments/944593144798932992/1132800475763331102/DOWNSCALING_TEST.png"
+      ],
+      "x": 1323,
+      "y": 1924
+    },
+    {
+      "name": "Another Deltarune",
+      "sources": [
+        "https://media.discordapp.net/attachments/1131353707053846550/1133038214576353300/image.png"
+      ],
+      "x": 1800,
+      "y": 1966
+    }
+  ],
+  "whitelist": [],
+  "blacklist": []
+}
