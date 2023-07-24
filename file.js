9452e9d4385359df168ddf06cfb3d3a360d7a432
9bg <99905695+wattoux@users.noreply.github.com>
2023-07-24 16:27:26 +0200

    Update file.js

--- file.js
+++ file.js
@@ -4,7 +4,7 @@
     {
       "name": "deltarune",
       "sources": [
-        "https://media.discordapp.net/attachments/944593144798932992/1132800475763331102/DOWNSCALING_TEST.png"
+        "https://media.discordapp.net/attachments/944593144798932992/1133010393887936625/DOWNSCALING_TEST.png"
       ],
       "x": 1823,
       "y": 1924
