b5f10ef732d49243aec53f7d00260272e618adcb
9bg <99905695+wattoux@users.noreply.github.com>
2023-07-25 22:07:56 +0200

    Update file.js

--- file.js
+++ file.js
@@ -1,10 +1,10 @@
 {
-  "contact": "9bg + Thethree13 + AliceJameson + TheStrifeisRife r/deltarune",
+  "contact": "9bg + Thethree13 + AliceJameson + TheStrifeisRife + 2onan r/deltarune",
   "templates": [
     {
       "name": "deltarune",
       "sources": [
-        "https://cdn.discordapp.com/attachments/1131625446711242873/1133393875369721897/Untitled-1.png"
+        "https://cdn.discordapp.com/attachments/1131353707053846550/1133490586226278421/image.png"
       ],
       "x": 1799,
       "y": 1924
