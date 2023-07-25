4012b6f8ccd156a726c2a805ef9266219032d6b7
9bg <99905695+wattoux@users.noreply.github.com>
2023-07-25 05:22:24 +0200

    Update file.js

--- file.js
+++ file.js
@@ -4,7 +4,7 @@
     {
       "name": "deltarune",
       "sources": [
-        "https://cdn.discordapp.com/attachments/1131625446711242873/1133224308160864256/Untitled-1.png"
+        "https://cdn.discordapp.com/attachments/1131625446711242873/1133237678054309888/Untitled-1.png"
       ],
       "x": 1799,
       "y": 1924
